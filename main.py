c4735574a379838eaaea52987cb81c6176dd0e11
Julio Toribio <88701917+juliotoribio@users.noreply.github.com>
2024-06-09 16:25:23 -0500

    Adicion de Boton para mandar a GPT

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, flash
 import re
 from youtube_transcript_api import YouTubeTranscriptApi
 
